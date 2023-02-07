fea675b3faede0bbedc74742cec6d9b21d1fe96d
0x7c00（咕咕） <110319858+DevOpsMogul@users.noreply.github.com>
2023-02-07 13:10:32 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var time = 6;
+var timer = setInterval(function(){
+    time--;
+    if(time <= 0){
+        clearInterval(timer);
+        document.title = "正在跳转..."
+        window.location.href = "moss.html";
+    }
+    document.querySelector(".time_h1").innerHTML = time;
+    document.querySelector(".time_h3").innerHTML = time;
+    if(time > 2){
+        document.title = "跳转倒计时：" + time + "秒";
+    }
+    if(time < 2){
+        document.querySelector(".distance_en_us_3").innerHTML = "SECOND";
+    }
+},1000);
