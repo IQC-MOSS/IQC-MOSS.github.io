8a94d49b42fbb39d02083f83fa8728f087790f17
skilfulwriter <42916413+skilfulwriter@users.noreply.github.com>
2023-02-13 04:38:22 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const viewCenter = (id)=>{
+	for(let i=0;i<document.getElementById('mainPanel').children.length;i++){
+		document.getElementById('mainPanel').children[i].style.display = 'none'
+	}
+	document.getElementById(id).style.display = 'block'
+}
+
+const closeAll = ()=>{
+	window.open('https://lib.cnvtn.com/temp/index.html', 'self', '');
+    window.close();
+}
